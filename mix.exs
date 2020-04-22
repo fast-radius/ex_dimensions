ba0735e5faf94deef64a113b215587c6db35a34e
Aaron Brenzel <aaron.brenzel@fastradius.com>
2020-04-22 16:02:00 -0500

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Units.MixProject do
   def project do
     [
       app: :ex_dimensions,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
